a73dcb36c6798f43205e06a64bcaa4b852353c9b
vaibhav-2002 <83127324+vaibhav-2002@users.noreply.github.com>
2023-01-27 23:25:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import AllUserList from "./component/AllUserList";
+import Test from "./Test";
+function App() {
+  return (
+    <div className="App">
+      {/* <Test></Test>0 */}
+      <AllUserList></AllUserList>
+    </div>
+  );
+}
+
+export default App;
